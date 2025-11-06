3498e3e55556d4928d33025c1caf851047e67818
刘大龙 <68359238+liudalongs@users.noreply.github.com>
2025-11-06 23:25:34 +0800

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 import os
 os.environ["WANDB_MODE"] = "disabled"
 import warnings, os
-import sys
 # os.environ["CUDA_VISIBLE_DEVICES"]="-1"    # 代表用cpu训练 不推荐！没意义！ 而且有些模块不能在cpu上跑
 # os.environ["CUDA_VISIBLE_DEVICES"]="0"     # 代表用第一张卡进行训练  0：第一张卡 1：第二张卡
 # 多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
@@ -37,7 +36,7 @@ if __name__ == '__main__':
     model.train(data='VisDrone2019yolo.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=300,
+                epochs=1,
                 batch=24,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
@@ -50,4 +49,3 @@ if __name__ == '__main__':
                 project='runs',
                 name='exp',
                 )
-     sys.exit()
