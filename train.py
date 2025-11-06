94af6052a16a8a9ab1bf9979fcf480a1ca2b39b7
刘大龙 <68359238+liudalongs@users.noreply.github.com>
2025-11-07 00:07:05 +0800

    Update train.py

--- train.py
+++ train.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
     model.train(data='VisDrone2019yolo.yaml',
                 cache=False,
                 imgsz=640,
-                epochs=1,
+                epochs=300,
                 batch=24,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
