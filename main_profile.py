7696f30f7fafb7c51a06841e4b688d3d66772fb9
liu <2910787880@qq.com>
2026-03-13 21:39:42 +0800

    3-13

--- main_profile.py
+++ main_profile.py
@@ -4,7 +4,7 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # choose your yaml file
-    model = YOLO('ultralytics/cfg/models/12/yolo12-ALL_AAttn_FULL.yaml')
+    model = YOLO('ultralytics/cfg/models/12/yolo12-CSP-PMSFA.yaml')
     model.info(detailed=True)
     try:
         model.profile(imgsz=[640, 640])
--- train.py
+++ train.py
@@ -28,14 +28,14 @@ from ultralytics import YOLO
 # YOLO13配置文件路径：ultralytics/cfg/models/13 预训练权重在这里下:https://github.com/iMoonLab/yolov13
 
 if __name__ == '__main__':
-    model = YOLO('ultralytics/cfg/models/12/yolo12-CSP-PMSFA.yaml') # YOLO11
+    model = YOLO('ultralytics/cfg/models/11/yolo11-C2PSA-EDFFN.yaml') # YOLO11
     #model=YOLO('runs/train/yolo12-ALL_AAttn_FULL.yaml/weights/last.pt')  #断点续训 下面resume=True要打开
     # model.load('yolo11n.pt') # loading pretrain weights
     model.train(data='VisDrone2019yolo.yaml',
                 cache=False,
                 imgsz=640,
                 epochs=400,
-                batch=32,
+                batch=24,
                 close_mosaic=0, # 最后多少个epoch关闭mosaic数据增强，设置0代表全程开启mosaic训练
                 workers=4, # Windows下出现莫名其妙卡主的情况可以尝试把workers设置为0
                 # device='0,1', # 指定显卡和多卡训练参考<YOLOV11配置文件.md>下方常见错误和解决方案
@@ -46,5 +46,5 @@ if __name__ == '__main__':
                 # fraction=0.2,
                 #accumulate=4, # 梯度累计次数，默认是1，建议根据显存情况设置，一般显存足够大可以设置为4-8
                 project='runs',
-                name='yolo12-CSP-PMSFA',
+                name='yolo11-C2PSA-EDFFN',
                 )
